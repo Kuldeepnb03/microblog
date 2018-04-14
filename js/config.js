636bc57e076d53a5de186805d88a010fc5329a97
Kuldeepnb03 <kuldeep@throughbit.com>
2018-04-14 14:32:32 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 
 var contract = '0x523cad65896abe1e2cbb790678c0db8a06fd67c7';
 var maxPosts = 5;
-var twitter = 'SwaroopH';
-var facebook = 'SwaroopHegde';
-var github = 'SwaroopH';
+var twitter = 'Kuldeep';
+var facebook = 'kuldeep';
+var github = 'kuldeepnb03';
 var prefix = 'https://alpha-api.ethvigil.com/contract/' + contract;
